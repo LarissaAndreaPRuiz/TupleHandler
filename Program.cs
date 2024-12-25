fad59c924618573815a6c3645b649a86fa20a4b7
Larissa Ruiz <80786453+LarissaAndreaPRuiz@users.noreply.github.com>
2024-12-25 16:00:40 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace UserDataProcessing.Models;
+
+class Program
+{
+    static void Main()
+    {
+        var handler = new TupleHandler();
+        
+        var userData = handler.GetUserData();
+        
+        handler.DisplayUserInfo(userData);
+    }
+}
